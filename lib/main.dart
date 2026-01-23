fcd50ebf326ffce78ce7c71575fad97074f210ca
Ahmed ElQady <ahmedzakaria3390@gmail.com>
2026-01-23 21:25:18 +0200

    handel structure and create splash screen

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:final_project/features/Splash%20screen/presentation/views/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Mokhi());
+}
+
+class Mokhi extends StatelessWidget {
+  const Mokhi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mokhi App',
+      home:SplashScreen(),
+    );
+  }
+}
