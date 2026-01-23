f058c5439d70181c79c08af5bd30ec22e0635319
Ahmed ElQady <ahmedzakaria3390@gmail.com>
2026-01-23 21:48:55 +0200

    add colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/animation.dart';
+
+Color primaryColor = const Color(0xff2563EB);
+Color gradientColorTop = const Color(0xff2563EB);
+Color gradientColorMiddle = const Color(0xff3B82F6);
+Color gradientColorBottom = const Color(0xff4F46E5);
+Color scaffoldBackgroundColorRegister = const Color(0xffEFF6FF);
+Color scaffoldBackgroundColorLogin = const Color(0xffE5E7EB);
+Color buttonColor = const Color(0xff3B82F6);
