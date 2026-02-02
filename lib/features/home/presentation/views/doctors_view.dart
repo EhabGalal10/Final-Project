06d12b455e4596e7a1bf42fa9961dc60154d04b0
Ahmed ElQady <ahmedzakaria3390@gmail.com>
2026-02-02 18:38:35 +0200

    refactor find doctor view

--- lib/features/home/presentation/views/doctors_view.dart
+++ lib/features/home/presentation/views/doctors_view.dart
@@ -1,8 +1,7 @@
-import 'package:final_project/core/utils/app_colors.dart';
 import 'package:final_project/core/utils/app_text_styles.dart';
 import 'package:final_project/features/home/data/models/Doctors_model.dart';
+import 'package:final_project/features/home/presentation/widgets/custom_doctor_card.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 
 class DoctorsView extends StatelessWidget {
   const DoctorsView({super.key});
@@ -22,62 +21,7 @@ class DoctorsView extends StatelessWidget {
         itemCount: DoctorsModel.doctors.length,
         itemBuilder: (context, index) {
           final DoctorsModel doctor = DoctorsModel.doctors[index];
-          return Card(
-            margin: const EdgeInsets.only(bottom: 12),
-            elevation: 4,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: ListTile(
-              contentPadding: const EdgeInsets.all(16),
-              leading: CircleAvatar(
-                radius: 28,
-                backgroundColor: Colors.blue.shade100,
-                child: const Icon(Icons.person, color: Colors.blue, size: 30),
-              ),
-              title: Text(
-                doctor.name!,
-                style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 6),
-                  Row(
-                    children: [
-                      const Icon(
-                        Icons.location_on,
-                        size: 16,
-                        color: Colors.grey,
-                      ),
-                      const SizedBox(width: 4),
-                      Text(doctor.address!),
-                    ],
-                  ),
-                  const SizedBox(height: 4),
-                  Row(
-                    children: [
-                      const Icon(Icons.phone, size: 16, color: Colors.grey),
-                      const SizedBox(width: 4),
-                      Text(doctor.phone.toString()),
-                    ],
-                  ),
-                ],
-              ),
-              trailing: IconButton(
-                icon: const Icon(Icons.call, color: AppColors.primaryColor),
-                onPressed: () async {
-                  // await launch('tel://${doctor.phone}');
-                  await FlutterPhoneDirectCaller.callNumber(
-                    doctor.phone.toString(),
-                  );
-                },
-              ),
-            ),
-          );
+          return CustomDoctorCard(doctor: doctor);
         },
       ),
     );
---
+++ lib/features/home/presentation/widgets/custom_doctor_card.dart
@@ -0,0 +1,57 @@
+import 'package:final_project/core/utils/app_colors.dart';
+import 'package:final_project/features/home/data/models/Doctors_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
+
+class CustomDoctorCard extends StatelessWidget {
+  const CustomDoctorCard({super.key, required this.doctor});
+  final DoctorsModel doctor;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 12),
+      elevation: 4,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      child: ListTile(
+        contentPadding: const EdgeInsets.all(16),
+        leading: CircleAvatar(
+          radius: 28,
+          backgroundColor: Colors.blue.shade100,
+          child: const Icon(Icons.person, color: Colors.blue, size: 30),
+        ),
+        title: Text(
+          doctor.name!,
+          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 6),
+            Row(
+              children: [
+                const Icon(Icons.location_on, size: 16, color: Colors.grey),
+                const SizedBox(width: 4),
+                Text(doctor.address!),
+              ],
+            ),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                const Icon(Icons.phone, size: 16, color: Colors.grey),
+                const SizedBox(width: 4),
+                Text(doctor.phone.toString()),
+              ],
+            ),
+          ],
+        ),
+        trailing: IconButton(
+          icon: const Icon(Icons.call, color: AppColors.primaryColor),
+          onPressed: () async {
+            // await launch('tel://${doctor.phone}');
+            await FlutterPhoneDirectCaller.callNumber(doctor.phone.toString());
+          },
+        ),
+      ),
+    );
+  }
+}
